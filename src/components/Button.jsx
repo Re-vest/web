241837ede1f2eb28a27b4034017d8216c87b3461
PatrickRodrigues19 <patrick.rodrigues@sptech.school>
2024-10-21 09:59:00 -0300

    Alterações Modal de cadastro

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
-export function Button({ text, icon, secondary = false }) {
+export function Button({ text, icon, secondary = false, style }) {
   return (
-    <button data-secondary={secondary} className="h-fit px-8 py-3 flex items-center gap-3 bg-yellow-600 rounded-lg text-blue-950 data-[secondary=true]:bg-blue-500 data-[secondary=true]:text-white">
+    <button style={style} data-secondary={secondary} className="h-fit px-8 py-3 flex items-center gap-3 bg-yellow-600 rounded-lg text-blue-950 data-[secondary=true]:bg-blue-500 data-[secondary=true]:text-white">
       {icon}
       {text}
+      
     </button> 
   )
 } 
