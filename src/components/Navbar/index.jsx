e5ce3c45b7006ca9e4cf8d11b60c8f09551711e6
PatrickRodrigues19 <patrick.rodrigues@sptech.school>
2024-10-29 14:31:59 -0300

    Alterações Css

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,31 +1,34 @@
-import { AlignEndHorizontal, Calendar, CircleDollarSign, Shirt, UsersRound } from "lucide-react";
+  import { AlignEndHorizontal, Calendar, CircleDollarSign, LogOut, Shirt, UsersRound } from "lucide-react";
 
-export function Navbar() {
-  return (
-    <div className="h-full w-14 bg-gray-500 py-6 flex flex-col items-center fixed" id="Navbar">
-      <a href="/dashboard" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
-        <AlignEndHorizontal size={24} />
+  export function Navbar() {
+    return (
+      <div className="h-full w-14 bg-gradient-to-b from-[#0D35FE] to-[#5CC3FF] py-6 flex flex-col items-center fixed space-y-8 justify-center" id="Navbar">
+        <a href="/dashboard" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+          <AlignEndHorizontal size={24} />
 
-      </a>
-      <a href="/estoque" className="relative w-full flex justify-center py-5 cursor-pointer hover:bg-white group">
-        <Shirt size={24} />
-        <span className="opacity-0 group-hover:opacity-100 absolute -right-14 bg-black rounded-md text-white p-1">aasd</span>
+        </a>
+        <a href="/estoque" className="relative w-full flex justify-center py-5 cursor-pointer hover:bg-white group">
+          <Shirt size={24} />
+          <span className="opacity-0 group-hover:opacity-100 absolute -right-14 bg-black rounded-md text-white p-1">aasd</span>
 
-      </a>
-      <a href="/eventos" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
-        <Calendar size={24} />
+        </a>
+        <a href="/eventos" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+          <Calendar size={24} />
 
-      </a>
+        </a>
 
-      <a href="equipe" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
-        <UsersRound size={24} />
+        <a href="equipe" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+          <UsersRound size={24} />
 
-      </a>
+        </a>
 
-      <a href="vendas" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
-        <CircleDollarSign size={24} />
+        <a href="vendas" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+          <CircleDollarSign size={24} />
+        </a>
 
-      </a>
-    </div>
-  )
-}
+        <a href="/" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+          <LogOut size={24} />
+        </a>
+      </div>
+    )
+  }
