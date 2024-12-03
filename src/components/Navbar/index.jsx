6ec9e9665839cecb00aed02774b7c626889fb944
Victor Hugo <victor.msantos@sptech.school>
2024-12-03 16:17:47 -0300

    responsividade dashboard pronta e ajustes na navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,35 +11,35 @@ import { useNavigate } from "react-router-dom";
     }
 
     return (
-      <div className="h-full w-14 bg-gradient-to-b from-[#0D35FE] to-[#5CC3FF] py-6 flex flex-col items-center fixed space-y-8 justify-center" id="Navbar">
-        <a href="/dashboard" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+      <div className="h-full w-14 bg-[#0D35FE] py-6 flex flex-col items-center fixed space-y-8 justify-center" id="Navbar">
+        <a href="/dashboard" className="w-full flex justify-center py-5 cursor-pointer hover:bg-yellow-500 text-white hover:text-black">
           <AlignEndHorizontal size={24} />
 
         </a>
-        <a href="/estoque" className="relative w-full flex justify-center py-5 cursor-pointer hover:bg-white group">
+        <a href="/estoque" className="relative w-full flex justify-center py-5 cursor-pointer hover:bg-yellow-500 text-white hover:text-black group">
           <Shirt size={24} />
           <span className="opacity-0 group-hover:opacity-100 absolute -right-14 bg-black rounded-md text-white p-1">Estoque</span>
 
         </a>
-        <a href="/eventos" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+        <a href="/eventos" className="w-full flex justify-center py-5 cursor-pointer hover:bg-yellow-500 text-white hover:text-black">
           <Calendar size={24} />
 
         </a>
 
         {sessionStorage.PERFIL !== 'ADMINISTRADOR' ? (<></>) : (
 
-        <a href="equipe" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+        <a href="equipe" className="w-full flex justify-center py-5 cursor-pointer hover:bg-yellow-500 text-white hover:text-black">
           <UsersRound size={24} />
 
         </a>
         ) }
 
 
-        <a href="vendas" className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+        <a href="vendas" className="w-full flex justify-center py-5 cursor-pointer hover:bg-yellow-500 text-white hover:text-black">
           <CircleDollarSign size={24} />
         </a>
 
-        <a onClick={handleExit} className="w-full flex justify-center py-5 cursor-pointer hover:bg-white">
+        <a onClick={handleExit} className="w-full flex justify-center py-5 cursor-pointer hover:bg-yellow-500 text-white hover:text-black">
           <LogOut size={24} />
         </a>
       </div>
