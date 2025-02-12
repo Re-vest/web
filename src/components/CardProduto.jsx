febf0d79a807da91a91ea1429323c48526bf669a
PatrickRodrigues19 <patrick.rodrigues@sptech.school>
2025-02-12 11:53:13 -0300

    Edições no carrinho

--- src/components/CardProduto.jsx
+++ src/components/CardProduto.jsx
@@ -1,5 +1,11 @@
 export function CardProduto({ image, nome, preco }) {
   return (
-    <div></div>
+    <div className="flex items-center p-4 bg-white shadow-md rounded-lg w-full">
+      <img src={image} alt={'foto'} className="w-16 h-16 rounded-lg" />
+      <div className="ml-4">
+        <p className="text-gray-700 font-medium">{nome}</p>
+        <p className="text-black font-bold">R$ {preco}</p>
+      </div>
+    </div>
   )
 }
