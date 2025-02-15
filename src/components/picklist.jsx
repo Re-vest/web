7a45bad2830167ccb62d34821ea30ed024e5ab8a
Patrick <patrick.rodrigues@sptech.school>
2025-02-14 21:22:15 -0300

    Trazendo eventos no carrinho

--- src/components/picklist.jsx
+++ src/components/picklist.jsx
@@ -3,7 +3,7 @@ import Select from 'react-select'
 const PickList = ({options, onChange, value}) => {
   return (
   <Select value={{ label: value, value: value }} placeholder="Selecionar" options={options} onChange={e => onChange(e.label)} styles={{
-    width: "100%",
+    width: "100%"
   }}/>
 )}
 export default PickList;
