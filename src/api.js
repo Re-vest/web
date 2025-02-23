301032c340f3a3ad534eb42ff494d3a10b07b466
Victor Hugo <victor.msantos@sptech.school>
2025-02-23 11:15:14 -0300

    arrumando bug do token

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACK_END,
   headers: {
-    'Authorization': `Bearer ${sessionStorage.TOKEN}`
+    'Authorization': `${sessionStorage.TOKEN && `Bearer ${sessionStorage.TOKEN}`}`
   }
 })
 
