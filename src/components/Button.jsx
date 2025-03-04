1458ae7db343642a68c4fc0d8cf3aeec0828da8e
rafascarabe <rafaela.scarabe@sptech.school>
2025-03-04 03:10:08 -0300

    Header e cards responsivos

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export function Button({ text, icon, secondary = false, onClick }) {
       onClick={onClick} 
       data-secondary={secondary} 
       className="
-        px-8 py-3 flex justify-center items-center gap-3 
+        px-6 py-2 flex justify-center items-center gap-3 
         bg-yellow-600 rounded-lg text-blue-950 
         data-[secondary=true]:bg-blue-500 
         data-[secondary=true]:text-white
@@ -14,8 +14,8 @@ export function Button({ text, icon, secondary = false, onClick }) {
         data-[secondary=true]:hover:text-blue-950
         duration-200
         text-nowrap
-        sm:px-4 sm:py-2 sm:text-sm 
-        md:px-6 md:py-2.5 md:text-base
+        md:px-7 md:py-2.5 sm:text-sm 
+        lg:px-8 lg:py-3 md:text-base
       "
     >
       {icon}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,19 @@ import Logo from '../assets/logo.png'
 
 export function Header() {
   return (
-    <header className="w-full px-24 py-10 flex items-center justify-between z-30">
-      <img src={Logo} alt="Logo escrito revest com uma roupa" className="w-20" />
-      <ul className="flex gap-24">
-        <li><a href="#about">Sobre nós</a></li>
-        <li><a href="#contato">Contato</a></li>
-      </ul>
+    <header className="w-full lg:px-24 px-15 lg:py-10 py-5 flex items-center justify-between lg:p-0 p-5 z-30">
+              <img src={Logo} alt="Logo escrito revest com uma roupa" className="w-14 lg:w-20" />
+              <ul className="gap-24 sm:gap-15 hidden md:flex">
+                <li><a href="#about">Sobre nós</a></li>
+                <li><a href="#contato">Contato</a></li>
+              </ul>
+        
+              <a href="/login">
+        
+              <Button text="Login" />
 
-      <a href="/login">
-
-      <Button text="Login" />
-      </a>
-
-    </header>
+              </a>
+        
+            </header>
   )
 }
